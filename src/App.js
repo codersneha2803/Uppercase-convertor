54c7b0e78be95276018aa8afa3ec426568ba6a37
Sneha Nikhade <sneha.nikhade28@gmail.com>
2024-05-09 23:38:39 +0530

    changes in bg color of nav and body

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Textform from './Components/Textform';
+import "./App.css";
+// import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import Textform from "./Components/Textform";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "#fff";
+    }
+  };
+
   return (
     <>
-      {/* <Navbar title="ZEVEN" /> */}
-      <Navbar/>
-      <div className='container my-3'>
-        <Textform heading = "Enter the text to analyze"/>
+      <Navbar
+        title="ZEVEN"
+        value2="AboutUs"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      {/* <Navbar/> */}
+      <div className="container my-3">
+        <Textform heading="Enter the text to analyze" mode={mode} />
+        {/* <About/> */}
       </div>
     </>
   );
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,28 +1,75 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function Textform(props) {
-    const [text, setText] = useState("Enter the text here");
+  const [text, setText] = useState("");
 
-    const handleUpClick = () =>{
-        console.log("converted to Uppercase" + text);
-        let newText = text.toUpperCase();
-        setText(newText)
-    }
+  const handleUpClick = () => {
+    // console.log("converted to Uppercase" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleOnchange = (event) =>{
-        console.log("status changed");
-        setText(event.target.value)
-    }
-    
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const handleOnchange = (event) => {
+    // console.log("status changed");
+    setText(event.target.value);
+  };
 
   return (
-    <div>
-        <h1>{props.heading}</h1>
+    <>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "black",
+        }}
+      >
+        <h1 className="">{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} id="myBox" onChange={handleOnchange} rows="8"></textarea>
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            onChange={handleOnchange}
+            rows="8"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "grey" : "white",
+            }}
+          ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>convert to Uppercase</button>
-    </div>
-    
-  )
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+          convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClClick}>
+          Clear
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{
+          color: props.mode === "dark" ? "white" : "black",
+        }}
+      >
+        <h2>Your Text Summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Enter the text above to preview"}</p>
+      </div>
+    </>
+  );
 }
